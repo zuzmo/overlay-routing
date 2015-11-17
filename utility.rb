51e864f6891d6d40179d05dfde0547ddeb4e9045
Gonzalo <gmartz21@gmail.com>
2015-11-16 19:30:35 -0500

    Utility class method to read the config file

---
+++ utility.rb
@@ -0,0 +1,20 @@
+class Utility
+
+	# ===============================================================
+	# Reads the config file and returns a Hash containing the 
+	# configuration options
+	# ===============================================================
+	def self.read_config(path)
+		options = Hash.new
+		File.open(path, 'r') do |f|
+			f.each_line do |line|
+				key, val = line.split('=')
+				key.strip!
+				val.strip!
+				options[key] = val
+			end
+		end
+		return options
+	end
+	
+end

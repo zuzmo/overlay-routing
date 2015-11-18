103fe80a66a06ba7294ed6d59ce219e5b82391d9
gonzalo <gmartz21@gmail.com>
2015-11-18 10:29:18 -0500

    Update server and client

--- client.rb
+++ client.rb
@@ -1,19 +1,53 @@
 require 'socket'
 
-hostname = 'localhost'
-port = 7000
+class Client
 
-s = TCPSocket.open(hostname, port)
+	def initilize(ip, port)
+		@server = TCPSocket.open(ip, port)
+		@request = nil
+		@response = nil
+		receive
+		send
+		@request.join
+		@response.join
+	end
+
+
+	def receive
+		@response = Thread.new do
+			loop {
+				msg = @server.gets.chomp
+				puts "#{msg}"
+			}
+		end
+	end
+
+
+	def send
+		@request = Thread.new do
+			loop {
+				msg = STDIN.gets.chomp
+				@server.pust(msg)
+			}
+		end
+	end
+end
+
+ip = '10.0.0.20'
+port = 7000
+s = Client.new(ip, port)
 
 # while line = gets
 # line = gets
 # puts line.chop
 # end
 
+# msg = 'hi'
+# s.puts(msg)
 
-while line = s.gets
-  print line
-end
+# while line = s.gets
+#   print line
+# end
 
 # s.send 'Hi from client'
 # STDOUT.flush
@@ -22,4 +56,4 @@ end
 # s.puts Thread.current.object_id
 
 
-s.close
+# s.close
--- server.rb
+++ server.rb
@@ -1,17 +1,57 @@
 require 'socket'
 
-Thread.new do
-  server = TCPServer.open(7000)
-  loop do
-    Thread.start(server.accept) do |client|
-      # puts Thread.current.object_id
-      client.puts(Time.now.ctime)
-      client.puts "Closing the connection. Bye!"
-      client.puts "Other line"
-      client.close
-    end
+class Server 
+
+  def initialize(node_name, port)
+    @node_name = node_name
+    @server = TCPServer.open(port)
+    @clients = Hash.new     # connections accepted by this server
+    @servers = Hash.new     # connections established to other servers
   end
+
+
+  def run
+    loop {
+      Thread.start(@server.accept) do |client|
+        listen_messages(client)
+      end
+    }
+  end
+
+
+  def listen_messages(client)
+    loop {
+      msg = client.gets.chomp
+      puts "#{msg}"
+    }
+    
+  end
+
+
 end
+
+
+server = Server.new('n1', 7000)
+server.run
+# t1 = Thread.new do
+#   server = TCPServer.open('10.0.0.20', 7000)
+#   puts server.addr
+#   loop do
+#     Thread.start(server.accept) do |client|
+      
+#       msg = client.gets.chomp
+#       puts "#{msg}"
+#       # puts server.addr
+#       # puts Thread.current.object_id
+#       # client.puts(Time.now.ctime)
+#       # client.puts "Closing the connection. Bye!"
+#       # client.puts "Other line"
+#       # client.close
+#     end
+#   end
+# end
+
+# t1.join
 
 
 
@@ -31,9 +71,9 @@ end
 # 	socket.close
 # end
 
-i = 0
-loop do
-  puts "And the script is still running (#{i})..."
-  i += 1
-  sleep 1
-end
+# i = 0
+# loop do
+#   puts "And the script is still running (#{i})..."
+#   i += 1
+#   sleep 1
+# end

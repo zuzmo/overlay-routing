65fd6e6948171096d58e3f33d3bd3956da7ae853
gonzalo <gmartz21@gmail.com>
2015-11-18 13:23:35 -0500

    read_link_method now returns two Hashes. Read the doc for more info

--- utility.rb
+++ utility.rb
@@ -1,14 +1,16 @@
 class Utility
 
-	# ===============================================================
+	# =========================================================================
 	# Reads the global config file and returns a Hash containing the
 	# configuration options
-	# ===============================================================
+	# =========================================================================
 	def self.read_config(path)
 		options = Hash.new
 		File.open(path, 'r') do |f|
 			f.each_line do |line|
-				key, val = line.split('=')
+				line.chomp!
+				next if line.empty?
+				key, val = line.split(',')
 				key.strip!
 				val.strip!
 				options[key] = val
@@ -17,20 +19,44 @@ class Utility
 		return options
 	end
 
-	# ===============================================================
+	# =========================================================================
 	# Reads the config file containing the cost for each of
-	# its outgoing links and returns a Hash with all the costs
-	# ===============================================================
-  def self.read_link_costs(path)
-		link_costs = Hash.new
+	# its outgoing links and returns two Hashes.
+	# 1st. Hash:
+	# 		maps node names to ip addresses (e.g. 'n1' => '10.0.0.20')
+	# 2nd. Hash:
+	#		maps ip address to a Hash of ip addresses with their costs 
+	#		(e.g. '10.0.0.20' => { '10.0.4.20' => 1 })
+	# =========================================================================
+	def self.read_link_costs(path)
+		hostname_ip_map = Hash.new
+		link_cost_map = Hash.new
 		File.open(path, 'r') do |f|
 			f.each_line do |line|
-				ip_address, port, cost = line.split(' ')
-				ip_address.strip!; port.strip!; cost.strip!
-				link_costs[ip_address] = {port => cost}
+				line.chomp!
+				next if line.empty?
+				node_a, ip_a, node_b, ip_b, cost = line.split(',')
+				node_a.strip!; ip_a.strip!; node_b.strip!; ip_b.strip!; cost.strip!
+				
+				# insert from node A to node B and viceversa
+				hostname_ip_map[node_a] = ip_a
+				hostname_ip_map[node_b] = ip_b
+
+				if link_cost_map.has_key?(ip_a)
+					sub_map = @link_cost_map[ip_a]
+					sub_map[ip_b] = cost
+				else
+					link_cost_map[ip_a] = {ip_b => cost}
+				end
+				if link_cost_map.has_key?(ip_b)
+					sub_map = @link_cost_map[ip_b]
+					sub_map[ip_a] = cost
+				else
+					link_cost_map[ip_b] = {ip_a => cost}
+				end
 			end
 		end
-		return link_costs
+		return hostname_ip_map, link_cost_map
 	end
 
 	# Returns the size of the specified string in bytes

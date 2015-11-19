2f978431fd767c5e0d3049290a902b340397498f
george2002 <menjivar2009@gmail.com>
2015-11-19 06:35:44 -0800

    added methods to main.rb and utility.rb

--- clock.rb
+++ clock.rb
@@ -3,22 +3,20 @@ class Clock
 
 	# =========================================================================
 	# This class gets the current time at initialization from the OS and 
-	# updates it when told to
+	# updates it when told to.
 	# =========================================================================
 
 
 	# =========================================================================
-	# Sets up the time constant and sets it to the OS time
+	# Sets up the time constant and sets it to the OS time.
 	# =========================================================================
 	def initialize
-
 		@time = Time.new
-
 	end
 
 
 	# =========================================================================
-	# Returns the current stored time
+	# Returns the current stored time.
 	# =========================================================================
 	def get_time
 		@time.inspect
@@ -26,9 +24,9 @@ class Clock
 
 	
 	# =========================================================================
-	# Updates the time
-	# 1st. seconds:
-	#        The amount of secods to add to the clock
+	# Updates the time.
+	# Params:
+	#        +seconds -> the amount of seconds to add to the clock
 	# =========================================================================
 	def tick(seconds)
 		@time += seconds
--- main.rb
+++ main.rb
@@ -1,58 +1,112 @@
 
 
 require_relative './utility.rb'
+require_relative './clock.rb'
 require 'thread'
 
-@debug_mode = true                                             #if true will print out debug messages
-@weights_file_name
-@update_interval
-@port = 7000
-@hook_pid
-
-def main
-				
-	 config_options = Utility.read_config("./s1/config")
-	 @update_interval = config_options["updateInterval"]
-	 @weights_file_name = config_options["weightFile"]
-     
-     @hook_pid = Thread.new{listen_for_hook}                    #initializes he thread that listens for hooks from stdin     
-     start_heartbeat()                                          #keeps meain thread alive
-
-end
+@debug_mode = true 
 
 
-def listen_for_hook
-	loop do	
-		user_input = gets.chomp                               #blocks while waiting for user input
+	# =========================================================================
+	# Starts the program.
+	# =========================================================================
+	def main
+		 read_config_file()			
+	     start_clock()
+	     listen_for_hook()
+	     start_heartbeat()
+	end
+
+
+	# =========================================================================
+	# creates the clock object that will get the time from the OS 
+	# and update it.
+	# =========================================================================
+	def start_clock
+	 	 @clock = Clock.new
+	end
+
+
+	# =========================================================================
+	# Reads the config file and sets up constants.
+	# =========================================================================	
+	def read_config_file
+		 config_options = Utility.read_config("./s1/config")
+		 @update_interval = config_options["updateInterval"]
+		 @weights_file_name = config_options["weightFile"]
+
+	end
+
+
+	# =========================================================================
+	# Starts the thread that will be in charge if listening for commands from 
+	# stdin.
+	# =========================================================================
+	def start_hook_listener
+	     @hook_pid = Thread.new{listen_for_hook}
+	end
+
+
+	# =========================================================================
+	# Listens and interprets commands given to stdin.
+	# =========================================================================
+	def listen_for_hook
+		loop do	
+			user_input = gets.chomp                               #blocks while waiting for user input
 		
-		case user_input
-		when /^DUMPSTABLE\s[\w\d\.]*/
-			file_name = user_input.split(" ")[1]
-			#todo
-		when /^FORCEUPDATE/
-			#todo
-		when /^CHECKSTABLE/
-			#todo
-		else
-			puts "try again"
+			case user_input
+			when /^DUMPTABLE\s[\w\d\.]*/
+				file_name = user_input.split(" ")[1]
+				dumptable(file_name)
+			when /^FORCEUPDATE$/
+				#todo
+			when /^CHECKSTABLE$/
+				#todo
+			else
+				puts "try again"
+			end
 		end
 	end
-end
 
 
-def start_heartbeat
-	loop do
-		sleep 1
+	# =========================================================================
+	# Keeps the main thread from dying. Updates the clock.
+	# =========================================================================
+	def start_heartbeat
+		loop do
+			sleep 1
+			@clock.tick(1)
+		end
 	end
-end
 
-def dbg(message)
-	if @debug_mode == true
-		puts message
+
+    # =========================================================================
+	# Retreives the current forwarding table from the forwarding table object 
+	# and writes it to a file.
+	# Params:
+	#        +file -> the destination file 
+	# =========================================================================
+	def dumptable(file)
+		#current_table = Forwarding_table.get_current_table
+		cc = "banana\n"
+		Utility.write_string_to_file(file,cc)
+
+		puts "wrote to #{file}"
+
 	end
-end
+
+	# =========================================================================
+	# Writes messages to stdout if the debug_mode constant is set to true.
+	# Params:
+	#        +message -> the message to print to console
+	# =========================================================================
+	def dbg(message)
+		if @debug_mode == true
+			puts message
+		end
+	end
 
 
-main                   #starts the main method
+main       
 
 

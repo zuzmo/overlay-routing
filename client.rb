3e1e9e0079cddf0c36a5312b945ee5db079c402d
gonzalo <gmartz21@gmail.com>
2015-11-22 17:19:07 -0500

    Fixed client

--- client.rb
+++ client.rb
@@ -36,38 +36,4 @@ class Client
 	def close
 		@server.close
 	end
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 931fc86e87725ae7fa9eb1d6b2f76dd0744f7a4c
 end
-
-# ip = '10.0.0.20'
-# port = 7000
-<<<<<<< HEAD
-# s = Client.new(ip, port)
-=======
-# s = Client.new(ip,port)
->>>>>>> 931fc86e87725ae7fa9eb1d6b2f76dd0744f7a4c
-
-# while line = gets
-# line = gets
-# puts line.chop
-# end
-
-# msg = 'hi'
-# s.puts(msg)
-
-# while line = s.gets
-#   print line
-# end
-
-# s.send 'Hi from client'
-# STDOUT.flush
-# msg = s.gets
-# print msg
-# s.puts Thread.current.object_id
-
-
-# s.close
